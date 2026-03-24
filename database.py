eeabd76ed8822287cdc07cfccdd75f929c12bc03
Priyanshu038 <b222038@iiit-bh.ac.in>
2026-03-24 20:55:08 +0530

    Create database.py

---
+++ database.py
@@ -0,0 +1,29 @@
+import os
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String, unique=True, index=True, nullable=False)
+    email = Column(String, unique=True, index=True, nullable=False)
+    phone_number = Column(String, nullable=False)
+    carrom_level = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+
+
+Base.metadata.create_all(bind=engine)
